03e36988c61413ceefb881d03d2a31e841537e21
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-03-11 16:59:38 -0700

    database finished

--- app/controllers/api/animals_controller.rb
+++ app/controllers/api/animals_controller.rb
@@ -1,2 +1,56 @@
 class Api::AnimalsController < ApplicationController
+  before_action :set_ranch, except: [:animals_all]
+    before_action :set_animal, only:[:show, :update, :destroy]
+    # give me all treaments of specific bug
+    # get 'api/bugs/:bug_id/treatments''
+    # let res = await axios.get('/api/bugs/1/treatments')
+    def index
+        render json: @ranch.animals
+    end
+
+    def animals_all
+        render json: Animal.all
+    end
+
+    # get on treatment of specific bug
+    # get 'api/bugs/:bug_id/treatments/:id''
+    # let res = await axios.get('/api/bugs/2/treatments/1')
+    def show
+        render json: @animal
+    end
+
+    def create
+      animal = @ranch.animals.new(animal_params)
+      if(animal.save)
+        render json: animal
+      else
+        render json: {errors: animal.errors.full_messages}, status: 422
+      end
+    end
+
+   
+    def update
+        if(@animal.update(animal_params))
+          render json: @animal
+        else
+          render json: {errors: @animal.errors.full_messages}, status: 422
+        end
+    end
+
+    def destroy
+        render json: @animal.destroy
+    end
+
+    private 
+    def set_ranch
+        @ranch = Ranch.find(params[:ranch_id])
+    end
+
+    def set_animal
+        @animal = @ranch.animals.find(params[:id])
+    end
+
+    def animal_params
+      params.require(:animal).permit(:kind, :price)
+    end
 end
--- app/controllers/api/ranches_controller.rb
+++ app/controllers/api/ranches_controller.rb
@@ -1,3 +1,44 @@
 class Api::RanchesController < ApplicationController
-    
+# PRETENT THIS WAS JUST DONE AS WELLL. BY SALLY
+before_action :set_ranch, only: [:update, :show, :destroy]
+
+def index
+  render json: Ranch.all
+end
+
+def show
+    render json: @ranch
+end
+
+def create
+  ranch = Ranch.new(ranch_params)
+  if(ranch.save)
+    render json: ranch
+  else
+    render json: {error:ranch.errors.full_messages}, status: 422
+  end
+end
+
+def update
+    if(@ranch.update(ranch_params))
+        render json: @ranch
+    else
+        render json: {error:@ranch.errors.full_messages}, status: 422
+    end
+end
+
+def destroy
+    render json: @ranch.destroy
+end
+
+private
+
+def set_ranch
+    @ranch = Ranch.find(params[:id])
+end
+
+def ranch_params
+  params.require(:ranch).permit(:name)
+end
+
 end
