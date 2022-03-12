580657ce3ab8e9855f08b67ba37b9ce6d87dcb76
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-03-12 11:15:39 -0700

    ranches done & added route to index

--- client/src/pages/Ranches.js
+++ client/src/pages/Ranches.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import RanchShow from './RanchShow'
 
 const Ranches = () => {
   const navigate = useNavigate()
@@ -10,7 +11,7 @@ const Ranches = () => {
     getRanches()
   })
 
-  const deleteBug = async (id) => {
+  const deleteRanch = async (id) => {
     await axios.delete(`/api/ranches/${id}`)
     let filteredRanches = ranches.filter(r => r.id !== id)
     setRanches(filteredRanches)
@@ -22,37 +23,22 @@ const Ranches = () => {
   }
 
 
+  const renderRanches = () => {
+    return ranches.map(r=> {
+        return <RanchShow key={r.id} {...r} deleteRanch={deleteRanch}/>
+    })
+ }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   const renderRanches = () => {
-//     return ranches.map(r=> {
-//         return <Ranch key={r.id} {...r} deleteRanch={deleteRanch}/>
-//     })
-//  }
-
-//   return (
-//     <div className='border'>
-//             <h1>Ranches</h1>
-//             <button onClick={()=>navigate('/ranches/new')}>new</button>
-//             <div>
-//                 {renderRanches()}
-//             </div>
-//             <p>{JSON.stringify(ranches)}</p>
-//         </div>
-//   )
-// }
+  return (
+    <div className='border'>
+            <h1>Ranches</h1>
+            <button onClick={()=>navigate('/ranches/new')}>new</button>
+            <div>
+                {renderRanches()}
+            </div>
+            <p>{JSON.stringify(ranches)}</p>
+        </div>
+  )
+}
 
 export default Ranches
