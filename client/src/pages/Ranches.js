a63863b52be693cee7cf4fa0072b177e2523a676
Sharayah Hefner <rocknrayah@gmail.com>
2022-03-12 11:15:24 -0700

    change logo added buttons

--- client/src/pages/Ranches.js
+++ client/src/pages/Ranches.js
@@ -21,38 +21,24 @@ const Ranches = () => {
     setRanches(res.data)
   }
 
+  const renderRanches = () => {
+    return ranches.map(r=> {
+        return <Ranch key={r.id} {...r} deleteRanch={deleteRanch}/>
+    })
+ }
+
+  return (
+    <div className='border'>
+            <h1>Ranches</h1>
+            <button onClick={()=>navigate('/ranches/new')}>new</button>
+            <div>
+                {renderRanches()}
+            </div>
+            <p>{JSON.stringify(ranches)}</p>
+        </div>
+  )
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   const renderRanches = () => {
-//     return ranches.map(r=> {
-//         return <Ranch key={r.id} {...r} deleteRanch={deleteRanch}/>
-//     })
-//  }
-
-//   return (
-//     <div className='border'>
-//             <h1>Ranches</h1>
-//             <button onClick={()=>navigate('/ranches/new')}>new</button>
-//             <div>
-//                 {renderRanches()}
-//             </div>
-//             <p>{JSON.stringify(ranches)}</p>
-//         </div>
-//   )
-// }
 
 export default Ranches
